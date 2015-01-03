1c742be86e54149ec658ddfcd87d6818836548a7
yusuf karacin <yusufkaracin@gmail.com>
2015-01-03 18:11:36 +0200

    english desc

--- 002_C_Dersleri/main.c
+++ 002_C_Dersleri/main.c
@@ -3,6 +3,7 @@
  * Author: Yusuf Karaçin
  *
  * Girilen kilometre değerinin kaç mil olduğunu hesaplayan program
+ * calculate mil to km
  */
 
 #include <stdio.h>
@@ -11,14 +12,14 @@
 
 int main(int argc, char** argv) {
     
-    double km; //kullanıcının gireceği kilometre değerini saklayacağım değişken
+    double km; //kilometre değerini saklayacağım değişken
     double mil = 0.6213; //1 mil = 0.6213 km
     
-    puts("Lütfen kilometre cinsinden bi değer giriniz:");
+    puts("Lütfen kilometre olarak bir değer giriniz:"); //input message
     scanf("%lf", &km);
     
     mil = km * mil;
-    printf("%lf km %lf mil yapar", km, mil);
+    printf("%lf km = %lf mil", km, mil);
 
     return (EXIT_SUCCESS);
 }
